272a689249561d38f361672af8d76163584a2c08
DimaAlabsi <86602837+DimaAlabsi@users.noreply.github.com>
2021-11-03 19:14:00 +0200

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 // Connects to our database depending on the URI as an environmental variable
-const POSTGRES_URI =   process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.HEROKU_POSTGRESQL_COBALT_URL  || process.env.DATABASE_URL ;
+const POSTGRES_URI =   process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.HEROKU_POSTGRESQL_GREEN_URL  || process.env.DATABASE_URL ;
 
  
 
